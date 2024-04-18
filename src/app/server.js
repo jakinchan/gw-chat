d6912e1b8aae7d4894a3a43fc899746a44a553c3
chin.shuji <chin.shuji@gmail.com>
2024-04-18 22:23:31 +0900

    main
    first commit
    
    Co-Authored-By: jakinchan <5602967+jakinchan@users.noreply.github.com>

---
+++ src/app/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on('connection', (socket) => {
+    console.log('New client connected');
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    });
+});
+
+const port = process.env.PORT || 4001;
+server.listen(port, () => console.log(`Listening on port ${port}`));
